831f4a3e6b8ec000b31fdef6e816da87dbb826c5
KheangEric <53136771+KheangEric@users.noreply.github.com>
2021-01-09 16:35:14 +0700

    index

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "February 11, 2021 12:00:00",
+	date: "December 31, 2021 12:59:59",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
